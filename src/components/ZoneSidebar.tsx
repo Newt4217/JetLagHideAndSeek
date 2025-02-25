9a8736bb3c300199eccb763c7ce0d1469d3ff26b
Newt42 <59628325+Newt4217@users.noreply.github.com>
2025-02-25 01:38:31 +0100

    Update ZoneSidebar.tsx

--- src/components/ZoneSidebar.tsx
+++ src/components/ZoneSidebar.tsx
@@ -413,7 +413,7 @@ export const ZoneSidebar = () => {
                                         },
                                         {
                                             label: "Bus Stops",
-                                            value: "[highway=tram_station]",
+                                            value: "[highway=tram_stop]",
                                         },
                                     ]}
                                     onValueChange={
